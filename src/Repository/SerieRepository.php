87319f9e34199d9cb548ded1b580a989a76a9cc7
gurun29 <98386819+gurun29@users.noreply.github.com>
2023-01-13 13:16:19 +0100

    fin demo module 5 queryBuilder

---
+++ src/Repository/SerieRepository.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Serie>
+ *
+ * @method Serie|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Serie|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Serie[]    findAll()
+ * @method Serie[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SerieRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Serie::class);
+    }
+
+    public function findBestSeries()
+    {
+        //en DQL
+        /*$entityManager = $this->getEntityManager();
+        $dql = "
+            SELECT s
+            FROM App\Entity\Serie s
+            WHERE s.popularity>100
+            AND s.vote>8
+            ORDER BY s.popularity DESC 
+        ";
+        $query = $entityManager->createQuery($dql);
+        */
+
+        //en version queryBuilder
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder->andWhere('s.popularity > 100');
+        $queryBuilder->andWhere('s.vote>8');
+        $queryBuilder->addOrderBy('s.popularity','ASC');
+        $query = $queryBuilder->getQuery();
+
+
+        $query->setMaxResults(50);
+        $result = $query->getResult();
+        return $result;
+    }
+
+    public function add(Serie $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Serie $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Serie[] Returns an array of Serie objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('s.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Serie
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
